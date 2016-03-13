7e49de0c02e36bed7a5e9ed55cb2c99615a40870
Michael Jeffrey Wu <michaeljeffreywu@gmail.com>
2016-03-12 22:44:26 -0800

    [feature] smiley on hover and php mail script

--- assets/js/smiley.js
+++ assets/js/smiley.js
@@ -2,8 +2,9 @@ var smiley = document.querySelector(".emoji1");
 var mouth = document.querySelector(".mouth");
 
 smiley.onmouseover = function() {
-	normalState();
-	setTimeout(function(){ happyState(); }, 3000);
+	happyState();
+	// normalState();
+	// setTimeout(function(){ happyState(); }, 3000);
 	
 }
 smiley.onmouseout = function() {
@@ -15,19 +16,19 @@ function happyState () {
 	// classie.add(mouth, "smile");
 
 	classie.remove(smiley, "emoji1");
-	classie.add(smiley, "emoji2");
+	classie.add(smiley, "happy");
 }
 function unhappyState () {
-	classie.remove(smiley, "emoji2");
+	classie.remove(smiley, "happy");
 	classie.add(smiley, "emoji1");
 }
 
-function normalState() {
-	classie.remove(smiley, "emoji1");
-	classie.remove(smiley, "emoji2");
-	classie.add(smiley, "normal");
+// function normalState() {
+// 	classie.remove(smiley, "emoji1");
+// 	classie.remove(smiley, "emoji2");
+// 	classie.add(smiley, "normal");
 
-	classie.remove(mouth, "mouth");
-	// classie.add(mouth, "normal-mouth");
+// 	classie.remove(mouth, "mouth");
+// 	// classie.add(mouth, "normal-mouth");
 
-}
+// }
