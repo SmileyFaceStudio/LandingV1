3edb1c18297d38db4e919ed6b6bf230db976a6f4
Michael Jeffrey Wu <michaeljeffreywu@gmail.com>
2016-03-16 00:06:13 -0700

    [feature] responsive background

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,13 +1,12 @@
-var livePatern = {
-  canvas: null,
-  context: null,
-  cols: 0,
-  rows: 0,
-  colors: [222, 251, 249, 248, 241, 240],
-  triangleColors: [],
-  destColors: [],
+  var canvas = null;
+  var context = null;
+  var cols = 0;
+  var rows = 0;
+  var colors = [222, 251, 249, 248, 241, 240];
+  var triangleColors = [];
+  var destColors = [];
   
-  init: function(){
+  function init(){
     this.canvas = document.getElementById('canvas');
     this.context = this.canvas.getContext('2d');
     this.cols = Math.floor(document.body.clientWidth / 24);
@@ -18,15 +17,15 @@ var livePatern = {
     
     this.drawBackground();
     this.animate();
-  },
+  };
 
-  resizeCanvas: function() {
-    htmlCanvas.width = window.innerWidth;
-    htmlCanvas.height = window.innerHeight;
-    redraw();
-  },
+  // resizeCanvas: function() {
+  //   htmlCanvas.width = window.innerWidth;
+  //   htmlCanvas.height = window.innerHeight;
+  //   redraw();
+  // },
   
-  drawTriangle: function(x, y, color, inverted){
+  function drawTriangle(x, y, color, inverted){
     inverted = inverted == undefined ? false : inverted;
 
     this.context.beginPath();
@@ -36,13 +35,13 @@ var livePatern = {
     this.context.fillStyle = "rgb("+color+","+color+","+color+")";
     this.context.fill();
     this.context.closePath();
-  },
+  };
   
-  getColor: function(){    
+  function getColor(){    
     return this.colors[(Math.floor(Math.random() * 6))];
-  },
+  };
   
-  drawBackground: function(){
+  function drawBackground(){
     var eq = null;
     var x = this.cols;
     var destY = 0;
@@ -59,9 +58,9 @@ var livePatern = {
         this.drawTriangle(x * 24, eq == 1 ? destY  : y * 24, this.getColor(), true);
       }
     }
-  },
+  };
   
-  animate: function(){
+  function animate(){
     var me = this;
 
     var x = Math.floor(Math.random() * this.cols);
@@ -77,7 +76,122 @@ var livePatern = {
     setTimeout(function(){    
       me.animate.call(me);
     }, 10);
-  },
-};
+  };
 
-!function(){livePatern.init();}()
+$(window).resize(function() {
+  // Dots = [];
+  // canvas.width = window.innerWidth;
+  // canvas.height = window.innerHeight;
+  init();
+});
+
+init();
+
+// var canvas = $('canvas')[0];
+// var context = canvas.getContext('2d');
+
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+
+// var Dots = [];
+// var colors = ['#FF9900', '#424242', '#BCBCBC', '#3299BB'];
+// var maximum = 70;
+
+// function Initialize() {
+//   GenerateDots();
+
+//   Update();
+// }
+
+// function Dot() {
+//   this.active = true;
+
+//   this.diameter = Math.random() * 7;
+
+//   this.x = Math.round(Math.random() * canvas.width);
+//   this.y = Math.round(Math.random() * canvas.height);
+
+//   this.velocity = {
+//     x: (Math.random() < 0.5 ? -1 : 1) * Math.random() * 0.7,
+//     y: (Math.random() < 0.5 ? -1 : 1) * Math.random() * 0.7
+//   };
+
+//   this.alpha = 0.1;
+//   this.hex = colors[Math.round(Math.random() * 3)];
+//   this.color = HexToRGBA(this.hex, this.alpha);
+// }
+
+// Dot.prototype = {
+//   Update: function() {
+//     if(this.alpha < 0.8) {
+//       this.alpha += 0.01;
+//       this.color = HexToRGBA(this.hex, this.alpha);
+//     }
+
+//     this.x += this.velocity.x;
+//     this.y += this.velocity.y;
+
+//     if(this.x > canvas.width + 5 || this.x < 0 - 5 || this.y > canvas.height + 5 || this.y < 0 - 5) {
+//       this.active = false;
+//     }
+//   },
+
+//   Draw: function() {
+//     context.fillStyle = this.color;
+//     context.beginPath();
+//     context.arc(this.x, this.y, this.diameter, 0, Math.PI * 2, false);
+//     context.fill();
+//   }
+// }
+
+// function Update() {
+//   GenerateDots();
+
+//   Dots.forEach(function(Dot) {
+//     Dot.Update();
+//   });
+
+//   Dots = Dots.filter(function(Dot) {
+//     return Dot.active;
+//   });
+
+//   Render();
+//   requestAnimationFrame(Update);
+// }
+
+// function Render() {
+//   context.clearRect(0, 0, canvas.width, canvas.height);
+//   Dots.forEach(function(Dot) {
+//     Dot.Draw();
+//   });
+// }
+
+// function GenerateDots() {
+//   if(Dots.length < maximum) {
+//     for(var i = Dots.length; i < maximum; i++) {
+//       Dots.push(new Dot());
+//     }
+//   }
+
+//   return false;
+// }
+
+// function HexToRGBA(hex, alpha) {
+//   var red = parseInt((TrimHex(hex)).substring(0, 2), 16);
+//   var green = parseInt((TrimHex(hex)).substring(2, 4), 16);
+//   var blue = parseInt((TrimHex(hex)).substring(4, 6), 16);
+
+//   return 'rgba(' + red + ', ' + green + ', ' + blue + ', ' + alpha + ')';
+// }
+
+// function TrimHex(hex) {
+//   return (hex.charAt(0) == "#") ? hex.substring(1, 7) : h;
+// }
+
+// $(window).resize(function() {
+//   Dots = [];
+//   canvas.width = window.innerWidth;
+//   canvas.height = window.innerHeight;
+// });
+
+// Initialize();
